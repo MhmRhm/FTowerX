75a0c7bcd605b29f2049af0e007fb8bd5b65c495
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-01-08 08:29:13 +0800

    fix: change help menu

--- src/ftowerx/main.cpp
+++ src/ftowerx/main.cpp
@@ -2,7 +2,7 @@ void runTOHGame(size_t size);
 
 int main() {
   std::cout << "q: Exit, Esc: Cancel Move, Arrow Keys: Moving Disks. Enter "
-               "number of disk: ";
+               "number of disks: ";
   size_t size{};
   std::cin >> size;
   runTOHGame(size);
